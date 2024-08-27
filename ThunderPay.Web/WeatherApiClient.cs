70c0e05a0250a79d1add78154c3e24c12614d2a4
arax zaeimi <arax.zaeimi@gmail.com>
2024-08-27 19:53:17 -0400

    stylecop configs added

--- ThunderPay.Web/WeatherApiClient.cs
+++ ThunderPay.Web/WeatherApiClient.cs
@@ -23,7 +23,7 @@ public class WeatherApiClient(HttpClient httpClient)
     }
 }
 
-public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+public record WeatherForecast(DateOnly Date, int TemperatureC, string Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
